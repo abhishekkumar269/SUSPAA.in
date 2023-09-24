f20247d010d86ef9987e8fa94c771f2ce69d827c
abhishek <abhishekkumar269@gmail.com>
2023-09-24 18:39:12 +0530

    first blog

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ from django.utils.html import format_html
 class Category(models.Model):
     cat_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
-    description = models.TextField(max_length=500)
+    description = models.TextField(max_length=10000)
     image = models.ImageField(upload_to ='category/')
     url = models.CharField(max_length=50)
     add_date = models.DateTimeField(auto_now_add=True,null=True)
@@ -29,7 +29,7 @@ class Category(models.Model):
 class Post(models.Model):
     Post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    content = models.TextField(max_length=2000)
+    content = models.TextField(max_length=10000)
     image = models.ImageField(upload_to='post/')
     cat = models.ForeignKey(Category,on_delete=models.CASCADE)
     url = models.CharField(max_length=100)
